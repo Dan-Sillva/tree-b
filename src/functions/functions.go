ac1f3e38afc8cb49b209a39bee06f420abe6f30a
Dan-Sillva <danilo.official22@hotmail.com>
2022-06-01 17:54:03 -0300

    save for continue my work at home

--- src/functions/functions.go
+++ src/functions/functions.go
@@ -12,12 +12,32 @@ type Leaf struct {
 	pointers []*Leaf
 }
 
+type List struct {
+	init *Leaf;
+}
+
 func CreateLeaf() Leaf{
 	var leaf Leaf
 	leaf.values = make([]int, size)
 	leaf.pointers = make([]*Leaf, size+1)
 
 	return leaf
+}
+
+func DeclareLeaf(values []int, pointers []*Leaf) Leaf{
+	var leaf Leaf
+	leaf.values = values
+	leaf.pointers = pointers
+
+	return leaf
+}
+
+func Get(leaf *Leaf) ([]int, []*Leaf) {
+	return leaf.values, leaf.pointers
+}
+
+func GetListInit(list List) *Leaf{
+	return list.init
 }
 
 func recursePrint(leaf *Leaf) {
@@ -63,15 +83,63 @@ func AddValueToLeaf(leaf Leaf, value int) Leaf {
 		}
 	}
 	leaf.values = orderB(leaf.values);
-	return leaf
 
+	if leaf.values[3] != 0 {
+		return Split(leaf)
+	} else {
+		return leaf
+	}
+
+}
+
+func HasPointer(leaf *Leaf) bool {
+	return leaf.pointers[0] != nil;
+} 
+
+func CreateList(list List, vetor []int) List {
+	var leaf Leaf
+	leaf = CreateLeaf()
+
+	for _, value := range vetor {
+		if list.init != nil { 
+			// break
+			if HasPointer(list.init) {
+				values, pointers := Get(list.init)
+		
+				for block, interValue := range values {
+					isSmaller := (value <= interValue)
+					
+					if isSmaller {
+						leaf = DeclareLeaf(Get(pointers[block]))
+						
+
+
+						leaf = AddValueToLeaf(leaf, value)
+						pointers[block] = &leaf
+					}
+				}
+		
+			} else {
+				leaf = DeclareLeaf(Get(list.init))
+				leaf = AddValueToLeaf(leaf, value)
+				list.init = &leaf	
+			}
+	
+		} else { 
+			leaf := AddValueToLeaf(leaf, value)
+			list.init = &leaf;
+
+		}
+	}
+
+	return list
 }
 
 func CheckForSplit(leaf Leaf) bool {
 	return leaf.values[3] != 0
 }
 
-func Split(leaf Leaf) *Leaf{
+func Split(leaf Leaf) Leaf{
 	upLeaf := CreateLeaf()
 	leftLeaf := CreateLeaf()
 	rightLeaf := CreateLeaf()
@@ -85,5 +153,5 @@ func Split(leaf Leaf) *Leaf{
 	rightLeaf.values[0] = leaf.values[2]
 	rightLeaf.values[1] = leaf.values[3]
 
-	return &upLeaf
+	return upLeaf
 }
--- src/main.go
+++ src/main.go
@@ -1,42 +1,26 @@
 package main
 import (
-	"fmt"
 	F "workspace/functions"
 )
 
-type List struct {
-	init *F.Leaf;
-}
-
-var vetor = [9]int{12, 14, 3, 7, 8, 1, 9, 13, 10}
-var list List
-
-var leaf F.Leaf
+var vetor = []int{12, 14, 3, 7, 8, 1, 9, 13, 10}
+var list F.List
 
 func main() {
 
-	if list.init != nil { //				Tem folha?
-		fmt.Println("vetor", list.init)
-	} else { // 							Não ?
-		leaf = F.CreateLeaf()
-
-		leaf = F.AddValueToLeaf(leaf, vetor[0])
-		leaf = F.AddValueToLeaf(leaf, vetor[1])
-		leaf = F.AddValueToLeaf(leaf, vetor[2])
-		leaf = F.AddValueToLeaf(leaf, vetor[3])
-
-		fmt.Printf("\ncheck for split:%v\n", F.CheckForSplit(leaf))
-		if F.CheckForSplit(leaf) {
-			list.init = F.Split(leaf)
-		} else {
-			list.init = &leaf;
-		}
-
-		
-	}
-
-	F.Print(list.init)
+	list = F.CreateList(list, vetor)
+	F.Print(F.GetListInit(list))
 
 	// if F.Root(leaf) == nil {fmt.Println("ta nulo")}
 
 }
+
+
+// leaf = F.AddValueToLeaf(leaf, value)
+	
+// fmt.Printf("\ncheck for split:%v\n", F.CheckForSplit(leaf))
+// if F.CheckForSplit(leaf) { 
+// 	list.init = F.Split(leaf)
+// } else {
+// 	list.init = &leaf;
+// }
